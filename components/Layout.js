c349209f7bca44d662bb45bd95a2b5a682e53658
triciachow0212 <suyeechow0212@gmail.com>
2022-09-16 11:36:05 +0800

    Add key prop

--- components/Layout.js
+++ components/Layout.js
@@ -12,8 +12,11 @@ export default function Layout({ children }) {
       <header className="flex justify-between items-center h-24 font-bold mx-8 mb-4 xl:mx-24 border-b border-slate-200">
         <Link href="/">Tricia C.</Link>
         <div className="flex gap-x-10">
-          {navItems.map(item => (
-            <div className="hover:text-amber-400 transition ease-in-out capitalize">
+          {navItems.map((item, index) => (
+            <div
+              key={index}
+              className="hover:text-amber-400 transition ease-in-out capitalize"
+            >
               <Link href={`/${item}`}>{item}</Link>
             </div>
           ))}
